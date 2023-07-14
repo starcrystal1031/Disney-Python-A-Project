99ddd09b73ab566f30556a0ffe748305da598f04
MaxwellWegener <138020411+MaxwellWegener@users.noreply.github.com>
2023-07-14 14:43:18 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,3 +1,8 @@
-import pandas
-import numpy
-print("hello world")
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+DIS=pd.read_csv("DIS.csv")
+plt.savefig("DIS.png")
+x=np.linspace(2,13356,13356)
+plt.plot(x,DIS['Close'])
