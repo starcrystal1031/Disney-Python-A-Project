b39c6beb1f8634a55779a0e05fc66a6c54666170
MaxwellWegener <138020411+MaxwellWegener@users.noreply.github.com>
2023-07-14 14:47:30 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,8 +1,13 @@
+import matplotlib
 import pandas as pd
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
+plt.savefig("DISGraph.jpeg")
+plt.style.use('seaborn-whitegrid')
 DIS=pd.read_csv("DIS.csv")
-plt.savefig("DIS.png")
 x=np.linspace(2,13356,13356)
 plt.plot(x,DIS['Close'])
+plt.xlabel('Days sence opening which I will change to years later')
+plt.ylabel('Closing price')
+plt.title('Value of DIS stock over time')
+plt.savefig("Value of DIS over time.jpeg")
